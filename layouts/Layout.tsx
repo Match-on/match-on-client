791a7c997859032188da42f11488458667bc64d3
cho seonghoon <chsida655@gmail.com>
2022-03-19 03:17:42 +0900

    layout

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -2,18 +2,30 @@ import Head from "next/head";
 import React from "react";
 import Header from "./Header";
 import NavBar from "./sidebar/NavBar";
+import Topbar from "./topbar/Topbar";
+
+import styled from "@emotion/styled";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
+
+const MainContent = styled.div`
+  position: absolute;
+  width: calc(100% - 70px);
+  height: calc(100% - 50px);
+  left: 70px;
+  top: 50px;
+  background-color: #f2f7f7;
+`;
 
 export default function Layout({ children }: LayoutProps) {
   return (
     <React.Fragment>
       <Header />
       <NavBar />
-      {children}
-      {/* <div style={{ background: "red", width: "1535px", height: "720px" }}>jh</div> */}
+      <Topbar />
+      <MainContent>{children}</MainContent>
     </React.Fragment>
   );
 }
