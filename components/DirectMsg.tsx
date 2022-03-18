2f424fd4a340c0fef0955694558ce2ccb91efc00
cho seonghoon <chsida655@gmail.com>
2022-03-19 03:17:16 +0900

    Direct Message ~ing

---
+++ components/DirectMsg.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DirectMsg: React.FC = () => {
+  return <div style={{ width: "25px", height: "25px", backgroundColor: "gray", marginRight: "50px" }}></div>;
+};
+
+export default DirectMsg;
