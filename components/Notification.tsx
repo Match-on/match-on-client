a75b78f5050289d879f08375f3bdfce8a5a43c99
cho seonghoon <chsida655@gmail.com>
2022-03-19 03:17:29 +0900

    Notification ~ing

---
+++ components/Notification.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Notification: React.FC = () => {
+  return <div style={{ width: "25px", height: "25px", backgroundColor: "gray" }}></div>;
+};
+
+export default Notification;
