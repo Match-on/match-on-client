55009d23b0e6dc46c41156269a91e3f69f47c766
cho seonghoon <chsida655@gmail.com>
2022-03-19 03:18:24 +0900

    topbar setting

--- layouts/topbar/Topbar.tsx
+++ layouts/topbar/Topbar.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 
+import DirectMsg from "../../components/DirectMsg";
+import Notification from "../../components/Notification";
+
+import styled from "@emotion/styled";
+
+const Top = styled.div`
+  position: absolute;
+  width: calc(100% - 70px);
+  height: 50px;
+  top: 0;
+  left: 70px;
+  background-color: #f2f7f7;
+  border: 1px solid black;
+`;
+
+const TopWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  right: 0;
+  width: 200px;
+  height: 100%;
+`;
+
 const Topbar: React.FC = () => {
   return (
-    <div>
-      <div>df</div>
-      <div>df</div>
-    </div>
+    <Top>
+      <TopWrapper>
+        <DirectMsg />
+        <Notification />
+      </TopWrapper>
+    </Top>
   );
 };
 
