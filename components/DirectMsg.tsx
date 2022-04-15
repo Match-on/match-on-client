a44c78801fe0ebe5f6cf8126684e4876544e19a8
cho seonghoon <chsida655@gmail.com>
2022-04-16 03:03:56 +0900

    DirectMsg

--- components/DirectMsg.tsx
+++ components/DirectMsg.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+import DmIcon from "../public/topbarSVG/directmsg.svg";
+
 const DirectMsg: React.FC = () => {
-  return <div style={{ width: "25px", height: "25px", backgroundColor: "gray", marginRight: "50px" }}></div>;
+  return <DmIcon />;
 };
 
 export default DirectMsg;
