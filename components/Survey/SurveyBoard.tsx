28cbbf397254f13c36503214ae5cb6bd95c431af
cho seonghoon <chsida655@gmail.com>
2022-05-31 17:39:24 +0900

    survey page

---
+++ components/Survey/SurveyBoard.tsx
@@ -0,0 +1,54 @@
+import { useMemo, useState } from "react";
+import styled from "@emotion/styled";
+
+import TableComponent from "../Table/Table";
+
+const Container = styled.div`
+  width: 100%;
+  height: 80%;
+  background-color: #ffffff;
+`;
+
+const SurveyBoard = () => {
+  const columns = useMemo(
+    () => [
+      {
+        accessor: "name",
+        Header: "Name",
+      },
+      {
+        accessor: "email",
+        Header: "Email",
+      },
+      {
+        accessor: "phone",
+        Header: "Phone",
+      },
+    ],
+    []
+  );
+
+  const data = [
+    { class: "survey", id: "survey-125345", name: "abc", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+    { class: "survey", id: "survey-1234215", name: "abc", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+    { class: "survey", id: "survey-1234445", name: "abc", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+    { class: "survey", id: "survey-1254345", name: "abc", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+    { class: "survey", id: "survey-1236545", name: "abc", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+    { class: "survey", id: "survey-12365445", name: "abc", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+    { class: "survey", id: "survey-1234845", name: "abcd", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+    { class: "survey", id: "survey-1237845", name: "abcde", email: "dfs@dfsfdsf.com", phone: "000-0000-0000" },
+  ];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <Container>
+      <TableComponent columns={columns} data={data} handleInputOpen={handleModalOpen} />
+      {isOpen && <></>}
+    </Container>
+  );
+};
+
+export default SurveyBoard;
--- pages/survey/index.tsx
+++ pages/survey/index.tsx
@@ -1,7 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+
+import NewSurvey from "../../public/componentSVG/survey/newSurvey.svg";
+import OldSurvey from "../../public/componentSVG/survey/oldSurvey.svg";
+import SurveyBoard from "../../components/Survey/SurveyBoard";
+import MySurvey from "../../components/Survey/MySurvey";
+
+const SurveyPage = styled.div`
+  position: absolute;
+  width: calc(100% - 8%);
+  height: 100%;
+  margin-left: 4%;
+  /* height: 100%; */
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 2.75em;
+  display: flex;
+  align-items: center;
+  margin-bottom: 5em;
+`;
+
+const Title = styled.div`
+  padding: 0 0.625em 0 0.625em;
+  font-size: 1.5rem;
+  font-weight: 400;
+  border-left: 0.25em solid #50d5d5;
+  text-align: center;
+  cursor: pointer;
+`;
+
+const SubTitle = styled.div`
+  font-size: 0.75rem;
+  font-weight: 400;
+  color: #aaaaaa;
+`;
+
+const MainContent = styled.div`
+  width: 100%;
+  height: 100%;
+  font-size: 1rem;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 70%;
+  margin-top: -1%;
+  padding-top: 1%;
+`;
+
+const Tab = styled.div`
+  width: 100%;
+  height: 2.5em;
+  font-size: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 768px) {
+    font-size: 0.5rem;
+  }
+  /* @media screen and (max-width: 450px) {
+    font-size: 0.2rem;
+  } */
+`;
+
+const TabMenu = styled.div<{ clicked: boolean }>`
+  width: calc(100% / 2);
+  height: 100%;
+  line-height: 2.5em;
+  text-align: center;
+  font-size: 1em;
+  font-weight: 400;
+  background-color: ${(props) => (props.clicked ? "#ffffff" : "#F1F7F7")};
+  color: ${(props) => (props.clicked ? "#000000" : "#aaaaaa")};
+  border-bottom: ${(props) => (props.clicked ? "#ffffff" : "0.15em solid #47d2d2")};
+  border-top: ${(props) => (props.clicked ? "0.15em solid #47d2d2" : "")};
+  border-left: ${(props) => (props.clicked ? "0.15em solid #47d2d2" : "")};
+  border-right: ${(props) => (props.clicked ? "0.15em solid #47d2d2" : "")};
+  border-radius: ${(props) => (props.clicked ? "0.625em 0.625em 0 0" : "")};
+  cursor: pointer;
+  &:hover {
+    color: #000000;
+    background-color: white;
+    border: 0.15em solid #47d2d2;
+    border-radius: 0.625em 0.625em 0 0;
+    border-bottom: none;
+  }
+`;
+
+const tabContArr = [
+  { tabNumber: 0, tabTitle: "설문조사 게시판" },
+  { tabNumber: 1, tabTitle: "내가 쓴 설문조사" },
+];
+
+const TabItem = ({ title, index, tab, handleTabMenu }) => {
+  return (
+    <TabMenu onClick={() => handleTabMenu(index)} clicked={index === tab}>
+      {title}
+    </TabMenu>
+  );
+};
 
 const survey: React.FC = () => {
-  return <div>Survey</div>;
+  const [tab, setTab] = useState(0);
+  const router = useRouter();
+  const handleTabMenu = (index) => {
+    console.log(`${index}clicked`);
+    setTab(index);
+  };
+
+  return (
+    <SurveyPage>
+      <Header>
+        <Title onClick={() => handleTabMenu(0)}>설문조사 게시판</Title>
+      </Header>
+      <MainContent>
+        <Tab>
+          {tabContArr.map((v, index) => (
+            <TabItem title={v.tabTitle} index={index} tab={tab} handleTabMenu={handleTabMenu} key={`tab=${index}`} />
+          ))}
+        </Tab>
+        <Container>
+          {tab === 0 && <SurveyBoard />}
+          {tab === 1 && <MySurvey />}
+        </Container>
+      </MainContent>
+    </SurveyPage>
+  );
 };
 
 export default survey;
