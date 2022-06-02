39f2bc8ac6684e5731869b5e7605aa447a723eb7
cho seonghoon <chsida655@gmail.com>
2022-06-03 00:08:19 +0900

    jwt token refresh

---
+++ components/auth/refreshTokenHandler.ts
@@ -0,0 +1,17 @@
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
+const RefreshTokenHandler = (props) => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (!!session) {
+      const timeRemaining = Math.round((Number(session.accessTokenExpiry) - 30 * 60 * 1000 - Date.now()) / 1000);
+      props.setInterval(timeRemaining > 0 ? timeRemaining : 0);
+    }
+  }, [session]);
+
+  return null;
+};
+
+export default RefreshTokenHandler;
