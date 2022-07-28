10630a82d8367f893b3404d798f39ec398d37adc
cho seonghoon <chsida655@gmail.com>
2022-07-28 21:27:10 +0900

    infinite scroll observer

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,34 @@
+import { RefObject, useEffect, useState } from "react";
+
+function useIntersectionObserver(
+  targetRef: RefObject<Element>,
+  options: IntersectionObserverInit = {
+    threshold: 0,
+    root: null,
+    rootMargin: "0%",
+  }
+): IntersectionObserverEntry | undefined {
+  const [entry, setEntry] = useState<IntersectionObserverEntry>();
+
+  const isIntersecting = entry?.isIntersecting;
+
+  const updateEntry = ([entry]: IntersectionObserverEntry[]): void => {
+    setEntry(entry);
+  };
+
+  useEffect(() => {
+    const target = targetRef?.current; // DOM Ref
+
+    if (isIntersecting || !target) return;
+
+    const observer = new IntersectionObserver(updateEntry, options);
+
+    observer.observe(target);
+
+    return () => observer.disconnect();
+  }, [targetRef, options.threshold, options.root, options.rootMargin, isIntersecting]);
+
+  return entry;
+}
+
+export default useIntersectionObserver;
