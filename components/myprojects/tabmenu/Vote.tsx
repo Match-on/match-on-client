4a7eae8adf0cbcdb1bbf72d68268ec3075d84691
cho seonghoon <chsida655@gmail.com>
2022-08-22 23:56:30 +0900

    upload vote

--- components/myprojects/tabmenu/Vote.tsx
+++ components/myprojects/tabmenu/Vote.tsx
@@ -1,58 +1,202 @@
-import React, { useMemo, useState } from "react";
 import styled from "@emotion/styled";
-import TableComponent from "../../sub/Table/Table";
-import VoteModal from "./TabContents/Input/VoteInputModal";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useMemo, useState } from "react";
+import { API_URL } from "../../api/API";
+import { VoteRow } from "../components/PostRow";
+import VoteModal from "./tabComponents/Modal/VoteModal";
+
+interface Vote {
+  voteIdx: number;
+  title: string;
+  endTime: string;
+  isNew: number;
+  count: string;
+}
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 95%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: #ffffff;
 `;
 
-const Vote = () => {
-  const columns = useMemo(
-    () => [
-      {
-        accessor: "name",
-        Header: "Name",
-      },
-      {
-        accessor: "date",
-        Header: "Date",
-      },
-      {
-        accessor: "voted",
-        Header: "Voted",
-      },
-      {
-        accessor: "entire",
-        Header: "Entire",
-      },
-    ],
-    []
-  );
+const TopSection = styled.div`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2%;
+  .right-section {
+    width: 15%;
+    height: 58%;
+    min-width: 160px;
+    display: flex;
+    justify-content: space-between;
+    .button {
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 1rem;
+      color: #ffffff;
+      background: #47d2d2;
+      border-radius: 0.625rem;
+      cursor: pointer;
+    }
+  }
+`;
 
-  const data = [
-    { class: "vote", id: "vote-12345", name: "프로젝트 이름", date: "2022-05-30", voted: "2", entire: "4" },
-    { class: "vote", id: "vote-123456", name: "다음 회의 가능 날짜", date: "2022-05-01", voted: "3", entire: "4" },
-    { class: "vote", id: "vote-123451", name: "프로젝트 이름", date: "2022-05-30", voted: "2", entire: "4" },
-    { class: "vote", id: "vote-123452", name: "다음 회의 가능 날짜", date: "2022-05-01", voted: "3", entire: "4" },
-    { class: "vote", id: "vote-123458", name: "프로젝트 이름", date: "2022-05-30", voted: "2", entire: "4" },
-    { class: "vote", id: "vote-123458", name: "다음 회의 가능 날짜", date: "2022-05-01", voted: "3", entire: "4" },
-    { class: "vote", id: "vote-12345", name: "프로젝트 이름", date: "2022-05-30", voted: "2", entire: "4" },
-    { class: "vote", id: "vote-123454", name: "다음 회의 가능 날짜", date: "2022-05-01", voted: "3", entire: "4" },
-    { class: "vote", id: "vote-12345", name: "프로젝트 이름", date: "2022-05-30", voted: "2", entire: "4" },
-    { class: "vote", id: "vote-123459", name: "다음 회의 가능 날짜", date: "2022-05-01", voted: "3", entire: "4" },
-  ];
+const InputFilter = styled.input`
+  width: 40%;
+  min-width: 9.5rem;
+  height: 60%;
+  border-radius: 0.625rem;
+  padding-left: 1%;
+  border: none;
+  background: #f2f6f6;
+  color: #ababab;
+  &:focus {
+    outline: 2px solid #47d2d2;
+  }
+  ::-webkit-input-placeholder {
+    margin-left: 20px;
+  }
+`;
+
+const SortSelect = styled.div`
+  width: 4.5rem;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #989898;
+  font-size: 0.75rem;
+  cursor: pointer;
+`;
+
+const SortOption = styled.div`
+  position: absolute;
+  height: 3rem;
+  width: 4.5rem;
+  border-radius: 0.625rem;
+  background: #ffffff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  margin-top: 4%;
+  > li {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 1.5rem;
+    font-size: 0.75rem;
+    padding: 5%;
+    cursor: pointer;
+    &:hover {
+      color: #47d2d2;
+      > svg {
+        fill: #47d2d2;
+      }
+    }
+  }
+`;
+
+const Table = styled.div`
+  width: 100%;
+  height: 90%;
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* IE, Edge */
+  scrollbar-width: none; /* Firefox */
+  ::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera */
+  }
+  border: 1px solid black;
+`;
+
+const Vote = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const { projectIdx } = router.query;
+
+  const [voteList, setVoteList] = useState<Vote[]>([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [filterOpen, setFilterOpen] = useState<boolean>(false);
+  const [filter, setFilter] = useState<string>("latest");
+  const [keyword, setKeyword] = useState<string>("");
+  const [params, setParams] = useState({ type: "free", sort: "latest", keyword: "", cursor: null });
 
   const handleModalOpen = () => {
-    setIsOpen(!isOpen);
+    setIsOpen((prev) => !prev);
   };
+
+  const returnIcon = () => {
+    return <span style={{ color: "#47d2d2" }}>{filterOpen ? "▲" : "▼"}</span>;
+  };
+
+  const setFilterValue = (value) => {
+    setFilter(value);
+    setFilterOpen(false);
+  };
+
+  const getVoteList = async () => {
+    try {
+      const params = { sort: filter, keyword: keyword };
+
+      const response = await axios.get(API_URL + `teams/${projectIdx}/votes`, {
+        params: params,
+        headers: {
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+      });
+      console.log(response.data.result);
+
+      setVoteList(response.data.result);
+      // setPostList((prev) => [...prev, ...list]);
+      // setVoteList(list);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getVoteList();
+    console.log(voteList);
+  }, [session, isOpen, filter]);
   return (
     <Container>
-      <TableComponent columns={columns} data={data} handleInputOpen={handleModalOpen} upload="업로드" />
-      {isOpen && <VoteModal isOpen={isOpen} handleOpen={handleModalOpen} />}
+      <TopSection>
+        <InputFilter type="text" placeholder="수업명을 검색하세요" />
+        <div className="right-section">
+          <SortSelect onClick={() => setFilterOpen((prev) => !prev)}>
+            <span>분류 :</span>
+            {filter === "latest" ? (
+              <span style={{ color: "#47d2d2" }}>날짜</span>
+            ) : (
+              <span style={{ color: "#47d2d2" }}>인기</span>
+            )}
+            {returnIcon()}
+          </SortSelect>
+          {filterOpen && (
+            <SortOption>
+              <li onClick={() => setFilterValue("latest")}>날짜</li>
+              <li onClick={() => setFilterValue("popular")}>인기</li>
+            </SortOption>
+          )}
+          <div className="button" onClick={() => setIsOpen(true)}>
+            업로드
+          </div>
+        </div>
+      </TopSection>
+      <Table>
+        {voteList.map((v, i) => (
+          <VoteRow key={i} {...v} />
+        ))}
+      </Table>
+      {isOpen && <VoteModal isOpen={isOpen} handleOpen={handleModalOpen} projectIdx={projectIdx} />}
     </Container>
   );
 };
