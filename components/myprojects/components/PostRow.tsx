f85105cbd1ac48d6be957beeab655a4122f84844
cho seonghoon <chsida655@gmail.com>
2022-09-27 23:29:18 +0900

    add: table row

--- components/myprojects/components/PostRow.tsx
+++ components/myprojects/components/PostRow.tsx
@@ -23,9 +23,18 @@ interface MeetingProps {
   commentCount: string;
   createdAt: string;
   files: string[];
-  isNew: string;
+  isNew: number;
   name: string;
   noteIdx: number;
+  title: string;
+}
+interface DriveProps {
+  commentCount: string;
+  createdAt: string;
+  files: string[];
+  isNew: number;
+  name: string;
+  driveIdx: number;
   title: string;
 }
 const Row = styled.div`
@@ -53,11 +62,12 @@ const Column = styled.div<{ width: number }>`
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  color: #a6a6a6;
   .mint_color {
     color: #47d2d2;
   }
   .gray_color {
-    color: #aaaaaa;
+    color: #a6a6a6;
   }
   .icon_wrapper {
     display: flex;
@@ -71,6 +81,7 @@ const Column = styled.div<{ width: number }>`
 const TitleCol = styled(Column)`
   align-items: flex-start;
   flex-direction: column;
+  color: #000000;
   @media screen and (max-width: 700px) {
     display: flex;
     width: 100%;
@@ -92,11 +103,11 @@ const TitleCol = styled(Column)`
     }
     .col_is_new {
       width: 3.5rem;
-      height: 1.2rem;
+      height: 1.4rem;
       border-radius: 10px;
       background: #47d2d2;
       text-align: center;
-      line-height: 1.2rem;
+      line-height: 1.4rem;
       color: #ffffff;
       margin-left: 1rem;
     }
@@ -155,7 +166,75 @@ export const MeetingRow = (props: MeetingProps) => {
       <TitleCol width={50}>
         <div className="col_title">
           {props.title}
-          {props.isNew === "1" && <div className="col_is_new">new</div>}
+          {props.isNew === 1 && <div className="col_is_new">new</div>}
+        </div>
+        <div className="col_date">{getDateDifference()}</div>
+      </TitleCol>
+      <Column width={10}>{props.name}</Column>
+      <Column width={20}>
+        {format(parseISO(props.createdAt), "yyyy.MM.dd")}
+      </Column>
+      <Column width={20}>
+        <div className="icon_wrapper">
+          <DownloadIcon />
+        </div>
+        <div className="icon_wrapper">
+          <ChatIcon />
+        </div>
+        <div className="icon_wrapper">
+          <ShareIcon />
+        </div>
+      </Column>
+    </Row>
+  );
+};
+
+export const NoticeRow = (props) => {
+  console.log(props);
+
+  return (
+    <Row>
+      <TitleCol width={70}>
+        <div className="col_title">
+          {props.title}
+          {props.isNew === 1 && <div className="col_is_new">new</div>}
+        </div>
+      </TitleCol>
+      <Column width={10} className="gray_color">
+        {props.name}
+      </Column>
+      <Column width={20}>
+        {format(parseISO(props.createdAt), "yyyy.MM.dd")}
+      </Column>
+    </Row>
+  );
+};
+
+export const DriveRow = (props: DriveProps) => {
+  const getDateDifference = () => {
+    const diff = differenceInMinutes(
+      new Date(),
+      new Date(parseISO(props.createdAt))
+    );
+    if (diff / 60 >= 1 && diff / 60 < 24) {
+      return `${Math.floor(diff / 60)} 시간 전`;
+    }
+    if (diff / 60 >= 24) {
+      return `${Math.floor(diff / 1440)} 일 전`;
+    }
+    if (diff >= 1 && diff / 60 < 1) {
+      return `${diff} 분 전`;
+    }
+    if (diff < 1) {
+      return `방금 전`;
+    }
+  };
+  return (
+    <Row>
+      <TitleCol width={50}>
+        <div className="col_title">
+          {props.title}
+          {props.isNew === 1 && <div className="col_is_new">new</div>}
         </div>
         <div className="col_date">{getDateDifference()}</div>
       </TitleCol>
